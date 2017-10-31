79e1d3d8b09887d4c3ed4f9ef7698ed78129786d
Wilson Mui <wmui@umail.ucsb.edu>
2017-10-31 11:33:26 -0700

    changed SimpleGui1

--- src/edu/ucsb/cs56/drawings/wmui/SimpleGui1.java
+++ src/edu/ucsb/cs56/drawings/wmui/SimpleGui1.java
@@ -20,9 +20,9 @@ public class SimpleGui1 {
     public static void main (String[] args) {
 	JFrame frame = new JFrame() ;
 	
-	JButton button = new JButton("Click me") ;
+	JButton button = new JButton("Click me harder") ;
 	
-	java.awt.Color myColor = new java.awt.Color(204,255,000);   // R, G, B values.
+	java.awt.Color myColor = new java.awt.Color(20,255,000);   // R, G, B values.
 	button.setBackground(myColor);
 	button.setOpaque(true);
 	
